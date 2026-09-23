a6b33ad61d25ed753afdb312916807d6193c348c
dalves-gitbasg <dalves.info@gmail.com>
2026-09-23 16:06:59 -0300

    organizar mcp

--- homelab-mcp/app/server.py
+++ homelab-mcp/app/server.py
@@ -1,15 +1,20 @@
+# MCP Server
+# Third-party imports
+from mcp.server.fastmcp import FastMCP
+
 from app.config import Config
+
+# Local application imports
 from app.tools.deploy import register_deploy_tools
 from app.tools.docker import register_docker_tools
 from app.tools.finance import register_finance_tools
 from app.tools.health import register_health_tools
-from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP(
     Config.MCP_NAME,
     host=Config.MCP_HOST,
     port=Config.MCP_PORT,
-    #transport_security=security,
+    # transport_security=security,
 )
 
 register_health_tools(mcp)
